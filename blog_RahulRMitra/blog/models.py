030fa8c9643e8818dd1b70d411fae4dad249fe41
Rahul Mitra <Rahul.Mitra@gmail.com>
2019-11-12 19:42:16 -0500

    Adding the blog

---
+++ blog_RahulRMitra/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    """
+    Represents a blog post
+    """
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)  # Sets on create
+    updated = models.DateTimeField(auto_now=True)  # Updates on each saver models here.
